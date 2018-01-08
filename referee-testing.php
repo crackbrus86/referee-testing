88e66910d4da222cc8373715e0530ae01b75aff5
qa <qa@coaliance.com>
2018-01-08 18:27:34 +0200

    Global: TS project created

--- referee-testing.php
+++ referee-testing.php
@@ -5,5 +5,23 @@ Description: App for testing referees
 Version: 1.0
 Author: Salivon Eugene
 */
-echo "111";
+define('RT_DIR', plugin_dir_path(__FILE__));
+add_action("admin_menu", array("RefereeTesting", "initSettings"));
+
+class RefereeTesting {
+    public function initSettings(){
+        add_menu_page("Referee Testing", "Тестування", "manage_options", "referee-testing", array("RefereeTesting", "editQuestons"));
+    }
+
+    public function editQuestons(){
+        $plName = "Запитання до тестування";
+        $otput=<<<_END
+<div class="container-fluid">
+        <h2>$plName</h2>
+</div>
+_END;
+echo $otput;
+    }
+}
+
 ?>
